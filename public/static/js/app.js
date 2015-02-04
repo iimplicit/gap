98fc996f57d2011068629fc07cc2098cd3370ba6
dobestan <dobestan@gmail.com>
2015-02-04 14:56:36 -0500

    update surveyViewPage : request resources from API

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -89,8 +89,7 @@ app.controller("surveyViewPageController", ["$scope", "$http", "$routeParams",
       // http://localhost:3000/api/surveys/54d1374ea0f09231943ee5d2
       $http.get("http://localhost:3000/api/surveys/" + $routeParams.id).
         success(function(data){
-          // $scope.survey = data.survey;
-          console.log("success")
+          $scope.survey = data.survey;
         }).
         error(function(data){
           console.log("error")
